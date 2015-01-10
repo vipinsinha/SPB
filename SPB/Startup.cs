726ef895965ea06c2107fec98faa0759e1272a81
vipinsinha <vipin.sinha@hotmail.com>
2015-01-10 19:08:51 +0530

    Revert "Revert "hello world""
    This reverts commit 2416f5df0e69aa25af927a31b180adc70e5b5022.

---
+++ SPB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SPB.Startup))]
+namespace SPB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
